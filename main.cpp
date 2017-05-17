f2e14ee07724779cf820131240c362fcaec452c8
dyachan <diego.yachan@gmail.com>
2017-05-17 19:10:36 -0400

    add tests

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
-int main(int argc, char* argv[]){
-    return 0;
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "pulentclass.h"
+
+TEST_CASE( "Pulentclass can be used", "[PulentClass]" ) {
+
+    PulentClass pc(12);
+
+    REQUIRE( pc.getValue() == 12 );
+
+    SECTION( "add one must add one" ) {
+        REQUIRE( pc.addOne() == 13 );
+    }
+
+    SECTION( "add two must add two" ) {
+        REQUIRE( pc.addTwo() == 14 );
+    }
 }
